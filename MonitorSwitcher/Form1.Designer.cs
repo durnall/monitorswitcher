bbbd31601625cadf7df4b3cb87304025844a8fae
John Durnall <john@durnall.co.uk>
2018-05-17 20:56:01 +0100

    initial commit

--- MonitorSwitcher/Form1.Designer.cs
+++ MonitorSwitcher/Form1.Designer.cs
@@ -29,12 +29,198 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runOnStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Monitor Switcher";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.preferencesToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(136, 136);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem1.Text = "external";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.SetDisplayModeExternal);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem2.Text = "internal";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.SetDisplayModeInternal);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem3.Text = "extend";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.SetDisplayModeExtend);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem4.Text = "clone";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.SetDisplayModeDuplicate);
+            // 
+            // preferencesToolStripMenuItem
+            // 
+            this.preferencesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runOnStartupToolStripMenuItem});
+            this.preferencesToolStripMenuItem.Name = "preferencesToolStripMenuItem";
+            this.preferencesToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.preferencesToolStripMenuItem.Text = "preferences";
+            // 
+            // runOnStartupToolStripMenuItem
+            // 
+            this.runOnStartupToolStripMenuItem.Checked = true;
+            this.runOnStartupToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.runOnStartupToolStripMenuItem.Name = "runOnStartupToolStripMenuItem";
+            this.runOnStartupToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.runOnStartupToolStripMenuItem.Text = "run on startup";
+            this.runOnStartupToolStripMenuItem.Click += new System.EventHandler(this.UpdateStartupRegistry);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.quitToolStripMenuItem.Text = "quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.QuitApp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "external";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SetDisplayModeExternal);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "internal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SetDisplayModeInternal);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "extend";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SetDisplayModeExtend);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(45, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "clone";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.SetDisplayModeDuplicate);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(45, 151);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "quit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.QuitApp);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(45, 128);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(152, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Start when Windows starts";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.UpdateStartupRegistry);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(207, 193);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runOnStartupToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- MonitorSwitcher/Form1.cs
+++ MonitorSwitcher/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,135 @@ namespace MonitorSwitcher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateControlsFromRegStartUp();
+        }
+
+        private void UpdateControlsFromRegStartUp()
+        {
+            runOnStartupToolStripMenuItem.Click -= UpdateStartupRegistry;
+            checkBox1.CheckedChanged -= UpdateStartupRegistry;
+
+            runOnStartupToolStripMenuItem.Checked = IsApplicationInStartup();
+            checkBox1.Checked = runOnStartupToolStripMenuItem.Checked;
+
+            runOnStartupToolStripMenuItem.Click += UpdateStartupRegistry;
+            checkBox1.CheckedChanged += UpdateStartupRegistry;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void SetDisplayMode(DisplayMode mode)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = Path.Combine(Environment.SystemDirectory, "DisplaySwitch.exe");
+            switch (mode)
+            {
+                case DisplayMode.External:
+                    proc.StartInfo.Arguments = "/external";
+                    break;
+                case DisplayMode.Internal:
+                    proc.StartInfo.Arguments = "/internal";
+                    break;
+                case DisplayMode.Extend:
+                    proc.StartInfo.Arguments = "/extend";
+                    break;
+                case DisplayMode.Duplicate:
+                    proc.StartInfo.Arguments = "/clone";
+                    break;
+            }
+            proc.Start();
+        }
+
+        enum DisplayMode
+        {
+            Internal,
+            External,
+            Extend,
+            Duplicate
+        }
+
+        private void SetDisplayModeExternal(object sender, EventArgs e)
+        {
+            SetDisplayMode(DisplayMode.External);
+        }
+
+        private void SetDisplayModeInternal(object sender, EventArgs e)
+        {
+            SetDisplayMode(DisplayMode.Internal);
+        }
+
+        private void SetDisplayModeExtend(object sender, EventArgs e)
+        {
+            SetDisplayMode(DisplayMode.Extend);
+        }
+
+        private void SetDisplayModeDuplicate(object sender, EventArgs e)
+        {
+            SetDisplayMode(DisplayMode.Duplicate);
+        }
+
+        private void QuitApp(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void UpdateStartupRegistry(object sender, EventArgs e)
+        {
+            var isApplicationInStartup = IsApplicationInStartup();
+            if (isApplicationInStartup)
+            {
+                RemoveApplicationFromStartup();
+            }
+            else
+            {
+                AddApplicationToStartup();
+            }
+            UpdateControlsFromRegStartUp();
+        }
+
+        public static void AddApplicationToStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue("MonitorSwitcher", "\"" + Application.ExecutablePath + "\"");
+            }
+        }
+
+        public static void RemoveApplicationFromStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.DeleteValue("MonitorSwitcher", false);
+            }
+        }
+
+        public bool IsApplicationInStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                return key.GetValue("MonitorSwitcher") == null ? false : true;
+            }
         }
     }
 }
